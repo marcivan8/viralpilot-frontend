e8ffaab4ef3e5816928461d504532803af6964c7
Marc Ivan <mariojaris2@gmail.com>
2025-08-10 14:40:38 +0000

    fix(frontend): fix tailwindcss plugin name in PostCSS config and restructure Tailwind animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,21 +5,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
-  animation: {
-      gradient: 'gradient 20s ease infinite',
-      moveClouds: 'moveClouds 120s linear infinite',
-    },
-    keyframes: {
-      gradient: {
-        '0%': { backgroundPosition: '0% 50%' },
-        '50%': { backgroundPosition: '100% 50%' },
-        '100%': { backgroundPosition: '0% 50%' },
+    extend: {
+      animation: {
+        gradient: 'gradient 20s ease infinite',
+        moveClouds: 'moveClouds 120s linear infinite',
       },
-      moveClouds: {
-        '0%': { transform: 'translateX(-100%)' },
-        '100%': { transform: 'translateX(100%)' },
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+        moveClouds: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        }
       }
     }
-  }
+  },
+  plugins: [],
 }
